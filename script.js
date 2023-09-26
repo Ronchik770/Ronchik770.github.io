74e3b3ee315bb72899cacb220c2483ff29035aa4
Ronchik770 <146062924+Ronchik770@users.noreply.github.com>
2023-09-26 09:46:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const apiKey = 'e5e359243d1fc4f88d9ef7dd8c376102';
+
+const cityInput = document.getElementById('cityInput');
+const searchButton = document.getElementById('searchButton');
+const locationElement = document.querySelector('.location h2');
+const temperatureElement = document.querySelector('.weather p:first-of-type');
+const descriptionElement = document.querySelector('.weather p:last-of-type');
+
+searchButton.addEventListener('click', () => {
+    const cityName = cityInput.value;
+    if (cityName) {
+        getWeather(cityName);
+    } else {
+        alert('Введите название города.');
+    }
+});
+
+function getWeather(city) {
+    const lang = 'ru'; // Устанавливаем язык на русский
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=${lang}`)
+        .then(response => response.json())
+        .then(data => {
+            updateWeather(data);
+            cityInput.value = ''; // Очищаем поле ввода после запроса
+        })
+        .catch(error => console.error('Ошибка:', error));
+}
+
+function updateWeather(data) {
+    locationElement.textContent = `${data.name}, ${data.sys.country}`;
+    const temperatureCelsius = Math.round(data.main.temp - 273.15);
+    temperatureElement.textContent = `Температура: ${temperatureCelsius}°C`;
+    descriptionElement.textContent = `Описание: ${data.weather[0].description}`;
+}
+
+// Вызываем функцию getWeather() при загрузке страницы для начального прогноза (например, для Москвы)
+getWeather('Москва');
+
